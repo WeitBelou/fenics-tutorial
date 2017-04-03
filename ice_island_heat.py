3f7c57277335265319676b9cdbfefc67af2dc886
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-04-03 22:01:20 +0300

    Add simple poisson problem solver.

--- ice_island_heat.py
+++ ice_island_heat.py
@@ -1,2 +1,34 @@
+from fenics import *
+
+from dolfin.cpp.io import File
+from dolfin.cpp.mesh import UnitSquareMesh
+
+
+def dirichlet_boundary(V, u_D):
+    return DirichletBC(V, u_D, lambda x, on_boundary: on_boundary)
+
+
+def functional_space(mesh):
+    return FunctionSpace(mesh, 'P', 2)
+
+
 if __name__ == '__main__':
-    pass
+    island = UnitSquareMesh(8, 8)
+
+    V = functional_space(island)
+
+    u_D = Expression('1 + x[0] * x[0] + 2 * x[1] * x[1]', degree=2)
+    bc = dirichlet_boundary(V, u_D)
+    f = Constant(-6.0)
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    a = dot(grad(u), grad(v)) * dX
+    L = f * v * dX
+
+    u = Function(V)
+    solve(a == L, u, bc)
+
+    vtkFile = File('output/poisson/solution.pvd')
+    vtkFile << u
